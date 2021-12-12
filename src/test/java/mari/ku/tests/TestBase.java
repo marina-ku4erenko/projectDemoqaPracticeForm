fca199cc1a2e043bfc1e2b4f66b3841861aa0a7b
marina-ku4erenko <marina.ku4erenko@yandex.ru>
2021-12-13 01:39:12 +0300

    Добавила настройки селеноида

--- src/test/java/mari/ku/tests/TestBase.java
+++ src/test/java/mari/ku/tests/TestBase.java
@@ -1,14 +1,25 @@
 package mari.ku.tests;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 
 public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.browserSize = "1600x900";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+
+        Configuration.browserCapabilities = capabilities;
     }
 
 }
