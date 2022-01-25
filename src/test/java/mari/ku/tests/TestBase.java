5eba79fb9488b6d7f8ad78bbb8347f9c29a58fd9
marina-ku4erenko <marina.ku4erenko@yandex.ru>
2022-01-25 22:33:13 +0300

    базовый вариант домашки 9

--- src/test/java/mari/ku/tests/TestBase.java
+++ src/test/java/mari/ku/tests/TestBase.java
@@ -3,26 +3,20 @@ package mari.ku.tests;
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import io.qameta.allure.selenide.AllureSelenide;
-import mari.ku.config.CredentialsConfig;
 import mari.ku.helpers.Attach;
-import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
-
-import static java.lang.String.format;
 
 public class TestBase {
 
 
     @BeforeAll
     static void beforeAll() {
-        CredentialsConfig credentials = ConfigFactory.create(CredentialsConfig.class);
 
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
         Configuration.browserSize = "1600x900";
-        Configuration.remote = format("https://%s:%s@%s", credentials.login(), credentials.password(),
-                System.getProperty("url","selenoid.autotests.cloud/wd/hub/"));
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("enableVNC", true);
